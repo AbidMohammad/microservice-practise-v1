8a644bc2f4aeec44790706779a79b1a6995d93bf
abmohamm <mohd.92java@gmail.com>
2022-01-01 21:27:38 -0500

    adding loggers

--- config-server-service/src/main/java/com/app/springcloud/ConfigServerServiceApplication.java
+++ config-server-service/src/main/java/com/app/springcloud/ConfigServerServiceApplication.java
@@ -1,12 +1,17 @@
 package com.app.springcloud;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ConfigServerServiceApplication {
+	
+	private static Logger logger = LoggerFactory.getLogger(ConfigServerServiceApplication.class);
 
 	public static void main(String[] args) {
+		logger.info("<===================================== Starting Config-Server =====================================>");
 		SpringApplication.run(ConfigServerServiceApplication.class, args);
 	}
 
