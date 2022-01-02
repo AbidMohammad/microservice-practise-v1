c4d495373835576d3d57e593d5ac68f54b3eb5f3
abmohamm <mohd.92java@gmail.com>
2022-01-02 18:03:30 -0500

    Ribbon-Client Configuration changes

--- product-service/src/main/java/com/app/springcloud/restclients/CouponRestClient.java
+++ product-service/src/main/java/com/app/springcloud/restclients/CouponRestClient.java
@@ -1,5 +1,6 @@
 package com.app.springcloud.restclients;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +8,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.app.springcloud.entity.Coupon;
 
-@FeignClient("GATEWAY-SERVICE")
+//@FeignClient("GATEWAY-SERVICE")
+@FeignClient("COUPON-SERVICE")
+@RibbonClient("COUPON-SERVICE")
 public interface CouponRestClient {
 
 	@GetMapping("/couponapi/coupons/{code}")
