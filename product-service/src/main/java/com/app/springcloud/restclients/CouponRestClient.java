41fe59b293d767ddfd027e8ef066e06abe787950
abmohamm <mohd.92java@gmail.com>
2022-01-02 19:28:19 -0500

    zuul-proxy configuration changes

--- product-service/src/main/java/com/app/springcloud/restclients/CouponRestClient.java
+++ product-service/src/main/java/com/app/springcloud/restclients/CouponRestClient.java
@@ -9,10 +9,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.app.springcloud.entity.Coupon;
 
 //@FeignClient("GATEWAY-SERVICE")
-@FeignClient("COUPON-SERVICE")
+//@FeignClient("COUPON-SERVICE")
+@FeignClient("ZUUL-PROXY-SERVICE")
 @RibbonClient("COUPON-SERVICE")
 public interface CouponRestClient {
 
-	@GetMapping("/couponapi/coupons/{code}")
+//	@GetMapping("/couponapi/coupons/{code}")
+	@GetMapping("/coupon-service/couponapi/coupons/{code}")
 	ResponseEntity<Coupon> getCoupon(@PathVariable("code")String code);
 }
