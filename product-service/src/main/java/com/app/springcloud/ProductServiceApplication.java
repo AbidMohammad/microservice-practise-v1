2983b150129419d8b99566029f8f733488b84081
abmohamm <mohd.92java@gmail.com>
2022-01-03 13:21:39 -0500

    adding hystrix-support

--- product-service/src/main/java/com/app/springcloud/ProductServiceApplication.java
+++ product-service/src/main/java/com/app/springcloud/ProductServiceApplication.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
@@ -20,6 +21,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableHystrix
 @EnableEurekaClient
 @EnableFeignClients
 public class ProductServiceApplication {
